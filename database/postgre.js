5a30ee38b0ccea37da35a7aa4909c825bdaaf620
Hayden Nicholls <haynick8@gmail.com>
2018-09-17 14:43:57 +1000

    testing git

--- database/postgre.js
+++ database/postgre.js
@@ -4,7 +4,7 @@ var client = new pg.Client(connectionString);
 
 client.connect(function(err) {
   if(err) {
-    return console.error('could not connect to postgres', err);
+    return console.error('could not connect to  postgres', err);
   }
 		console.log("conect sep-postgresql");
   });
